99dc6f63eccd3edb88b22a3b11728b57ee4530c7
jj13679 <jpjerosch@gmail.com>
2025-09-01 17:29:27 -0300

    Commit de inicio de confecção da pagina de login

--- Src/View/FormLoginMain.pas
+++ Src/View/FormLoginMain.pas
@@ -4,17 +4,16 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls,
+  Vcl.Imaging.pngimage;
 
 type
   TFormLogin = class(TForm)
     Panel1: TPanel;
     Panel2: TPanel;
-    Panel3: TPanel;
-    bFormLoginSair: TButton;
-    Panel4: TPanel;
-    Panel5: TPanel;
+    Image1: TImage;
     procedure bFormLoginSairClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -33,4 +32,21 @@ begin
   FormLogin.Close;
 end;
 
+procedure TFormLogin.FormCreate(Sender: TObject);
+begin
+
+Image1.Stretch:=True;
+Image1.Proportional:=True;
+Image1.Center:=True;
+
+
+if WindowState = wsMaximized then begin
+  Image1.Width := 1000
+end else begin
+  Image1.Width := 800;
+end;
+
+
+
+end;
 end.
