dc2c9386d998f9b0709e3442caac1763f7942968
jj13679 <jpjerosch@gmail.com>
2025-09-02 17:20:17 -0300

    Commit Terminando a pagina de login

--- Src/View/FormLoginMain.pas
+++ Src/View/FormLoginMain.pas
@@ -5,13 +5,20 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls,
-  Vcl.Imaging.pngimage;
+  Vcl.Imaging.pngimage, Vcl.Imaging.jpeg;
 
 type
   TFormLogin = class(TForm)
+    pRight: TPanel;
+    lblLogin: TLabel;
+    bSair: TPanel;
+    bRegister: TPanel;
+    bLogin: TPanel;
     Panel1: TPanel;
-    Panel2: TPanel;
-    Image1: TImage;
+    iLoginLeft: TImage;
+    Shape1: TShape;
+    pEmailLogin: TPanel;
+    eEmail: TEdit;
     procedure bFormLoginSairClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
@@ -35,15 +42,21 @@ end;
 procedure TFormLogin.FormCreate(Sender: TObject);
 begin
 
-Image1.Stretch:=True;
-Image1.Proportional:=True;
-Image1.Center:=True;
+iLoginLeft.Stretch:=True;
+iLoginLeft.Proportional:=True;
+iLoginLeft.Center:=True;
 
 
 if WindowState = wsMaximized then begin
-  Image1.Width := 1000
+  iLoginLeft.Width := 1800;
+
+
+
+
 end else begin
-  Image1.Width := 800;
+
+
+  iLoginLeft.Width := 800;
 end;
 
 
